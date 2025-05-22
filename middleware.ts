d9efa20428aade18cabe4b1270ede2b80c5c6da3
C_11nJxx <c11njxxsmailbox@gmail.com>
2025-05-22 11:25:37 +0800

    handle some redirect with the state of login

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,32 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-up(.*)",
+  "/subscribe/(.*)",
+]);
+
+const isSignUpRoute = createRouteMatcher(["/sign-up(/*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const userAuth = await auth();
+  const { userId } = userAuth;
+  const { pathname, origin } = req.nextUrl;
+
+  // 判断是否在访问公共页面，或者是在登录状态访问非公共页面，如果是非登录状态访问非公共页面，则重定向
+  if (!isPublicRoute(req) && !userId) {
+    console.log("Access Denied");
+    return NextResponse.redirect(new URL("/sign-up", origin));
+  }
+  //判断是否在登录状态访问登录界面，若是则跳转到meal-plan页面
+  if (isSignUpRoute(req) && userId) {
+    return NextResponse.redirect(new URL("/meal-plan", origin));
+  }
+
+  //完成检查操作
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: [
