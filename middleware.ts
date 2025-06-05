3c55c8e37149b3d54da49c346945e8103b7aceda
C_11nJxx <c11njxxsmailbox@gmail.com>
2025-06-05 15:39:01 +0800

    设置访问mealplan页面对是否订阅的检查

--- middleware.ts
+++ middleware.ts
@@ -6,14 +6,22 @@ const isPublicRoute = createRouteMatcher([
   "/sign-up(.*)",
   "/subscribe/(.*)",
   "/api/webhook(.*)",
+  "/api/check-subscription(.*)",
 ]);
 
 const isSignUpRoute = createRouteMatcher(["/sign-up(/*)"]);
+
+const isMealPlanRoute = createRouteMatcher(["/meal-plan(/*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const userAuth = await auth();
   const { userId } = userAuth;
   const { pathname, origin } = req.nextUrl;
+
+  //避免循环
+  if (pathname === "/api/check-subscription") {
+    return NextResponse.next();
+  }
 
   // 判断是否在访问公共页面，或者是在登录状态访问非公共页面，如果是非登录状态访问非公共页面，则重定向
   if (!isPublicRoute(req) && !userId) {
@@ -23,6 +31,21 @@ export default clerkMiddleware(async (auth, req) => {
   //判断是否在登录状态访问登录界面，若是则跳转到meal-plan页面
   if (isSignUpRoute(req) && userId) {
     return NextResponse.redirect(new URL("/meal-plan", origin));
+  }
+  //判断是否访问mealplan页面,并已登录
+  if (isMealPlanRoute(req) && userId) {
+    //查看是否订阅?使用API Route(api/check-subscription)
+    try {
+      const response = await fetch(
+        `${origin}/api/check-subscription?userId=${userId}`
+      );
+      const data = await response.json();
+      if (!data.subscriptionActive) {
+        return NextResponse.redirect(new URL("/subscribe", origin));
+      }
+    } catch (error: any) {
+      return NextResponse.redirect(new URL("/subscribe", origin));
+    }
   }
 
   //完成检查操作
